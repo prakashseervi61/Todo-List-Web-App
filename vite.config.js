2341fb8522a8f99352a4d4f4e45a3fdca0d8925a
Prakash V <prakashseervi1503@gmail.com>
2025-08-03 20:11:46 +0530

    Add gh-pages deploy script and homepage

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Todo-List-Web-App/',  // set this to your repo name, with leading and trailing slash
   plugins: [react()],
 })
