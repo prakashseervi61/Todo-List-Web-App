171d758b1d43332e88eb9cb656691321f10180ae
Prakash V <prakashseervi1503@gmail.com>
2026-01-16 18:57:31 +0530

    installed deploy dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-})
+  base: "/Todo-List-Web-App/",
+  plugins: [react()],
+});
